c981a068b34b07cbc2302197deedb6986b78d5fc
aidilrx04 <mrcrayv14@gmail.com>
2025-03-04 05:57:10 +0800

    logout

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -40,5 +41,7 @@ class DatabaseSeeder extends Seeder
             PaymentSeeder::class,
             RatingSeeder::class
         ]);
+
+        Auth::logout();
     }
 }
