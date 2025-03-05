a1a6ffc42362751ce6e0f2863c57966e837a9032
aidilrx04 <mrcrayv14@gmail.com>
2025-03-05 14:20:54 +0800

    link storage

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\CartItemController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('unauthorized', function () {
@@ -35,4 +36,8 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('bulkSave', [CartItemController::class, 'store_bulk'])->name('bulkSave');
     });
     Route::apiResource('carts', CartItemController::class);
+});
+
+Route::get('storagelink', function () {
+    Artisan::call('storage:link');
 });
